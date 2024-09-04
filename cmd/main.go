72097b76da5937df15fe2a22a8748b2a21d2f515
David Hintelmann <davidhin@mac.com>
2024-09-04 01:40:15 -0400

    add gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/davidhintelmann/blockchain/bparser"
 )
@@ -9,7 +13,7 @@ import (
 const (
 	gensisBlock     = "6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000"
 	gensisBlockSwap = "000000000019D6689C085AE165831E934FF763AE46A2A6C172B3F1B60A8CE26F"
-	blocksFilePath  = "C:\\Users\\david\\OneDrive\\Documents\\code\\python\\Blockchain\\Bitcoin\\data\\bitcoin_data"
+	blocksFilePath  = "C:\\Users\\david\\OneDrive\\Documents\\code\\python\\Blockchain\\Bitcoin\\data\\bitcoin_data\\"
 )
 
 func main() {
@@ -22,4 +26,28 @@ func main() {
 
 	fmt.Println(bparser.ByteSwap(gensisBlock))
 	fmt.Println(bparser.ByteSwapArray(gensisBlock))
+
+	fmt.Println(filepath.Dir(blocksFilePath))
+
+	matches, err := filepath.Glob(blocksFilePath + "*.dat")
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	fmt.Println(len(matches))
+	fmt.Println(matches[0])
+	// for _, v := range matches {
+	// 	fmt.Println(v)
+	// }
+	file, err := os.Open(matches[0])
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	readAll, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	fmt.Println(readAll[:283])
 }
